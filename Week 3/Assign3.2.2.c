0f6a26e28710b7ab1607dbbfb4ccf38ca7665260
hamsaelhussieny <105558006+hamsaelhussieny@users.noreply.github.com>
2023-11-10 00:25:00 +0200

    Update Assign3.2.2.c

--- Week 3/Assign3.2.2.c
+++ Week 3/Assign3.2.2.c
@@ -32,3 +32,15 @@ int main() {
 
     return 0;
 }
+/**
+The size of the `database` struct is larger than the size of the previous union
+because  all members share the same memory space (union). 
+The size of the union is determined by the largest member. 
+In this case, the size of the union would be equal to the size of the largest structure,
+which is `category` with its two `int` members.
+but in a struct, each member has its own separate memory space.
+The size of the struct is the sum of the sizes of all its members. 
+Therefore, the size of the `database` struct in this example would be the sum of the sizes of `shops`,
+`product_info`, and `category` structures.
+**/
+
